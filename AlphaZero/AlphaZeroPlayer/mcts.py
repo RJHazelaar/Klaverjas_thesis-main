96cb6d2a1499e3d8704c376c3837d16f8eee7881
RJHazelaar <robert-jan-hazelaar@hotmail.com>
2024-03-06 04:00:40 +0100

    Update mcts.py

--- AlphaZero/AlphaZeroPlayer/mcts.py
+++ AlphaZero/AlphaZeroPlayer/mcts.py
@@ -185,7 +185,6 @@ class MCTS:
         return move
     
     def mcts_n_simulations(self, state: State, training: bool, extra_noise_ratio):
-        print("testje")
         legal_moves = state.legal_moves()
         if len(legal_moves) == 1:
             return next(iter(legal_moves))
