06e7ed82567dec4892f116b9e85b48f41ad87fcc
RJHazelaar <robert-jan-hazelaar@hotmail.com>
2024-06-25 18:22:48 +0200

    Update train_alphazero_policy.py

--- AlphaZero/train_alphazero_policy.py
+++ AlphaZero/train_alphazero_policy.py
@@ -123,7 +123,7 @@ def selfplay(mcts_params, model_path, bidding_model_path, num_rounds, extra_nois
             X_train_bid[round_num * 4 + player] = X_train_bid[round_num * 4 + player] * bidder
             y_train_bid[round_num * 4 + player] = alpha_player_0.state.get_prediction_score(0, round.declarer, round.trump_suit) * bidder
 
-    y_train = zip(y_train_value, y_train_policy)
+    y_train = y_train_value, y_train_policy
     train_data = np.concatenate((X_train, y_train), axis=1)
 
     # First remove bidding training data for players that didnt bid
